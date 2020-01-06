d0318981a355077ed9bd88c5425c8a5dc55857c6
mariamiah <mariah.kakande@gmail.com>
2020-01-06 11:02:49 +0300

    setup dash dashboard

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 ## Python Script to explore historical loans data
 Service that consumes a kaggle dataset on a monthly basis
 
@@ -5,6 +6,8 @@ Service that consumes a kaggle dataset on a monthly basis
 - Reads data from the dataset it receives
 - Aggregates the data and stores it in the database
 - Interactive time-driven dashboard that displays data aggregations per field, quality of data supplied by field
+- For the analysis, we will ascertain the top 25 historic IBRD borrower, we will determine their total loan disbursements, current loan obligations, and the average interest rates they were charged for all loans.
+- The results of the analysis, a  DataFrame containing 25 rows, will be saved as a CSV-format data file.
 
 ## Analysis
 Tool of choice: Python
@@ -21,7 +24,7 @@ the python CSV library for reading and organizing the loans data.
 ## Collecting data
 Source : CSV
 - The dataset contains statements of loans historical data updated on a monthly basis
-- Due to the large file size, the raw copy is uploaded to google drive here
+- Due to the large file size, the raw copy is can be located [here](https://www.kaggle.com/theworldbank/ibrd-statement-of-loans-data/data)
 - Download the dataset and place it in the same folder as the python script
 
 ## Visualization
@@ -33,4 +36,5 @@ Framework of choice: Dash
 -  Run the script using a Python integrated development environment (IDE) such as Spyder. 
 -  To install Spyder, you will need to download the Anaconda installer.  
 -  This script is written in Python 3, so you will need the Python 3.x version of the installer. After downloading and installing Anaconda, you will find the Spyder IDE by opening Anaconda Navigator.
+
 
--- app.py
+++ app.py
@@ -3,23 +3,36 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
+import numpy as np
+from datetime import datetime 
 import plotly.graph_objs as go
 
 # read the csv file
-df = pd.read_csv("ibrd-statement-of-loans-historical-data.csv")
+df = pd.read_csv("ibrd-statement-of-loans-historical-data.csv", low_memory = False, parse_dates=['End of Period'])
+
+# remove columns we donot need to make data processing faster
+df.drop(['Effective Date (Most Recent)', 'Closed Date (Most Recent)'], axis=1, inplace=True)
+
+# Basic stats
+def stats():
+    return html.Div([
+        html.Div("Number of records in the dataset: " + str(len(df))),
+        html.Div("Number of features in the dataset: " + str(len(df.columns))),
+))
+])
+
+# CLEANING AND PREPARING DATA
+# Convert dates to datetime
+df['End of Period'] = pd.to_datetime(df['End of Period']).dt.date.astype('datetime64[ns]')
+df['First Repayment Date'] = pd.to_datetime(df['First Repayment Date']).dt.date.astype('datetime64[ns]')
+df['Last Repayment Date'] = pd.to_datetime(df['Last Repayment Date']).dt.date.astype('datetime64[ns]')
+df['Agreement Signing Date'] = pd.to_datetime(df['Agreement Signing Date']).dt.date.astype('datetime64[ns]')
+df['Board Approval Date'] = pd.to_datetime(df['Board Approval Date']).dt.date.astype('datetime64[ns]')
+df['Last Disbursement Date'] = pd.to_datetime(df['Last Disbursement Date']).dt.date.astype('datetime64[ns]')
 
 # List all countries inorder to populate the dropdown
 
 ctry_options = df["Country"].unique()
-
-def generate_summary(dataframe):
-    return html.Table(
-        # Body
-        [html.Tr("Missing Values:"),
-        html.Tr("Maximum value:"),
-        html.Tr("Minimum value:")]
-    )
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # initialize the dash application
@@ -34,7 +47,7 @@ colors = {
 # set up the layout of the application
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H3(
-        children='Loans History Data - Awamo',
+        children='Loans History Data - IBRD',
         style={
             'textAlign': 'center',
             'color': colors['text']
@@ -45,7 +58,6 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         'textAlign': 'center',
         'color': colors['text']
     }),
-    
     html.Div(
         [
             dcc.Dropdown(
@@ -60,8 +72,10 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                'display': 'inline-block'}),
     dcc.Graph(
         id='funnel-graph'),
-    html.H5(children='Statistics on the quality of data supplied by field'),
-    generate_summary(df)
+    
+    html.H5(children='Basic Statistics'),
+    stats()
+
 ])
 
 # add interactivity to the application
@@ -77,13 +91,8 @@ def update_graph(Country):
         df_plot = df[df["Country"] == Country]
     
     # pivot table aggregates values by taking the sum
-    pv = pd.pivot_table(
-        df_plot,
-        index=["Region"],
-        columns=["End of Period"],
-        values=["Repaid to IBRD"])
-        
-    trace1 = go.Bar(x=pv.index, y=pv[('Quantity', 'Repaid')], name='Declined')
+    pv = pd.pivot_table(df_plot,index=["Project Name "], values=["Due to IBRD"], fill_value=0)
+    trace1 = go.Bar(x=pv.index, y=pv[('Due to IBRD')])
 
     return {
         'data': [trace1],
@@ -95,3 +104,5 @@ def update_graph(Country):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
